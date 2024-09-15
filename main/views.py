b4af904e9924256adc00e7b996ee4d1da068a78b
fakhrihabb <fakhrihabb@gmail.com>
2024-09-15 13:33:13 +0700

    Pembuatan html create product entry, penyesuaian main urls.py dan views.py

--- main/views.py
+++ main/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductEntryForm
+from main.models import Product
 def show_main(request):
+    product_entries = Product.objects.all()
     context = {
         'app_name': 'FawApparel',
         'name': 'Fakhri',
         'class': 'PBP C',
+        'product_entries': product_entries,
     }
 
     return render(request, 'main.html', context)
+
+def create_product_entry(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
 # Create your views here.
