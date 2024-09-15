76d0cf83aeb80bcbbd931b50a603763bae7cc3b2
fakhrihabb <fakhrihabb@gmail.com>
2024-09-15 14:34:25 +0700

    penambahan fitur lihat json

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product_entry, show_xml
+from main.views import show_main, create_product_entry, show_xml, show_json
 
 app_name = 'main'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product-entry', create_product_entry, name='create_product_entry'),
     path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
 ]
