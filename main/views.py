4fc13170e6c9b507d767dfad49daf42d3f7c6900
fakhrihabb <fakhrihabb@gmail.com>
2024-09-25 11:35:31 +0700

    Penyelesaian tugas 4

--- main/views.py
+++ main/views.py
@@ -1,26 +1,67 @@
 from django.shortcuts import render, redirect
 from main.forms import ProductEntryForm
 from main.models import Product
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.urls import reverse
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+      if form.is_valid():
+          user = form.get_user()
+          login(request, user)
+          response = HttpResponseRedirect(reverse('main:show_main'))
+          response.set_cookie('last_login', str(datetime.datetime.now()))
+          return response
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:show_main'))
+    response.delete_cookie('last_login')
+    return response
+
+@login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.all()
+    product_entries = Product.objects.filter(user=request.user)
     context = {
+        'name': request.user.username,
         'app_name': 'FawApparel',
-        'name': 'Fakhri',
         'class': 'PBP C',
         'product_entries': product_entries,
+        'last_login': request.COOKIES.get('last_login'),
     }
 
     return render(request, 'main.html', context)
 
 def create_product_entry(request):
     form = ProductEntryForm(request.POST or None)
-
     if form.is_valid() and request.method == 'POST':
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return redirect('main:show_main')
-
     context = {'form': form}
     return render(request, "create_product_entry.html", context)
 
